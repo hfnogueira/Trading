7e8b29b2ee504e8df56b4e33016b2bef750d08a4
HFN <hugo.nogueira@tlantic.com>
2018-10-11 11:59:23 +0100

    update

---
+++ 4 - Adding Rules and applying trading strategy.R
@@ -0,0 +1,10 @@
+## add rules to the strategy and apply strategy.
+## https://analyticsprofile.com/r-algo-trading-course/chapter-4-adding-rules-and-applying-trading-strategy-in-r/
+
+
+
+###########
+## Rules ##
+###########
+
+
